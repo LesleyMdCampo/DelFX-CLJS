1f3bc537076e836b423ca592c23d587c1e5af7cc
Lesley Martin del Campo <lmartindelcampo@pros.com>
2015-02-20 13:26:22 -0600

    Add pages for img onClick. Add events for type onClick

--- delfx-src/delfx/html.cljs
+++ delfx-src/delfx/html.cljs
@@ -1,7 +1,7 @@
 (ns delfx.html
   (:require-macros [hiccups.core :as hiccups])
   (:require
-    [delfx.dom :as dom]
+    [delfx.dom :refer [by-id set-html!]]
     [delfx.util :refer [js-log log]]
     [hiccups.runtime]
     [quiescent :include-macros true]
@@ -23,22 +23,51 @@
 ;; Events
 ;;------------------------------------------------------------------------------
 
-(defn- click-work-div [js-evt]
+(defn- click-work-type [js-evt]
   (let [link (aget js-evt "currentTarget" "id")
-        link-id ( str "#" link)]
-    )
-  )
+        link-id (str "#" link)]
+    (.hide ($ "img.round-img-242ac"))
+    (.show ($ (str "img.round-img-242ac." link)))))
 
 (defn- add-events []
-  (.on ($ ".type-circle-1caf1") "click" #(click-work-div %))
-
-  )
+  (.on ($ ".nav-link-a7aa8") "click" #(click-work-type %)))
 
 ($ add-events)
 
 ;;------------------------------------------------------------------------------
 ;; HTML
 ;;------------------------------------------------------------------------------
+
+(hiccups/defhtml category-circles [work]
+  [:div.type-circle-1caf1 {:id (:id work)}
+    [:a {:href (str "works/" (:id work) ".html")}
+      [:img
+        {:class (str "round-img-242ac " (:type work))
+         :src (:img work)}]]])
+
+(hiccups/defhtml works-circles []
+  [:div.types-circles-f5820
+    (map category-circles (works-data-structure))
+    [:div.clr-bc54a]])
+
+(hiccups/defhtml sticky-header []
+  [:div.sticky-header-d95b2
+    [:div.container-32dc6
+      [:div.header-links-a2c3d
+        [:a.nav-link-a7aa8.type1 {:id "works"} "Works"]
+        [:a.nav-link-a7aa8.type2 {:id "styleboards"} "Styleboards"]
+        [:a.nav-link-a7aa8.type3 {:id "storyboards"} "Storyboards"]
+        [:a.nav-link-a7aa8.type4 {:id "design"} "Graphic Design"]]]
+        [:div.clr-bc54a]
+        (works-circles)])
+
+(hiccups/defhtml about-section []
+  [:div.about-container-3e121
+    [:div.left-col-e4a63 [:span.underline-a552e "Bio"]
+      [:div.para-f88dc "Cat ipsum dolor sit amet, inspect anything brought into the house yet stick butt in face sit in box. Meowing non stop for food hide from vacuum cleaner yet plan steps for world domination sleep on keyboard hack up furballs kick up litter but hate dog. Sweet beast destroy couch, or stand in front of the computer screen."]]
+    [:div.right-col-d0849 [:span.underline-a552e "Purpose"]
+      [:div.para-f88dc "Stick butt in face bathe private parts with tongue then lick owner's face i like big cats and i can not lie roll on the floor purring your whiskers off for mark territory, but ears back wide eyed. Hide at bottom of staircase to trip human. Hate dog sun bathe, and spread kitty litter all over house and sweet beast, but burrow under covers, for stretch. Claw drapes stand in front of the computer screen use lap as chair, for sleep on keyboard all of a sudden cat goes crazy."]]
+    [:div.clr-bc54a]])
 
 (hiccups/defhtml social-icons []
   [:a.fa.fa-twitter {:href "https://twitter.com/dels_tweet" :data-username "dels_tweet" :target "_blank"}]
@@ -54,35 +83,6 @@
           [:div.title-fe5d6 "Daniel Martin del Campo"]
           [:div.about-1043c "Motion Graphics Designer"]
           [:div.social-43a6b (social-icons)]]]]
-    [:div.clr-bc54a]])
-
-(hiccups/defhtml about-section []
-  [:div.about-container-3e121
-    [:div.left-col-e4a63 [:span.underline-a552e "Bio"]
-      [:div.para-f88dc "Cat ipsum dolor sit amet, inspect anything brought into the house yet stick butt in face sit in box. Meowing non stop for food hide from vacuum cleaner yet plan steps for world domination sleep on keyboard hack up furballs kick up litter but hate dog. Sweet beast destroy couch, or stand in front of the computer screen."]]
-    [:div.right-col-d0849 [:span.underline-a552e "Purpose"]
-      [:div.para-f88dc "Stick butt in face bathe private parts with tongue then lick owner's face i like big cats and i can not lie roll on the floor purring your whiskers off for mark territory, but ears back wide eyed. Hide at bottom of staircase to trip human. Hate dog sun bathe, and spread kitty litter all over house and sweet beast, but burrow under covers, for stretch. Claw drapes stand in front of the computer screen use lap as chair, for sleep on keyboard all of a sudden cat goes crazy."]]
-    [:div.clr-bc54a]])
-
-(hiccups/defhtml sticky-header []
-  [:div.sticky-header-d95b2
-    [:div.container-32dc6
-      [:div.header-links-a2c3d
-        [:a.nav-link-a7aa8.type1 "Works"]
-        [:a.nav-link-a7aa8.type2 "Styleboards"]
-        [:a.nav-link-a7aa8.type3 "Storyboards"]
-        [:a.nav-link-a7aa8.type4 "Graphic Design"]]]
-        [:div.clr-bc54a]])
-
-(hiccups/defhtml category-circles [work]
-  [:div.type-circle-1caf1 {:id (:id work)}
-    [:img
-      {:class (str "round-img-242ac " (:type work))
-       :src (:img work)}]])
-
-(hiccups/defhtml works-circles []
-  [:div.types-circles-f5820
-    (map category-circles (works-data-structure))
     [:div.clr-bc54a]])
 
 (hiccups/defhtml contact-me []
@@ -104,10 +104,9 @@
     (header)
     [:div.main-container-dc45e
       (about-section)
-      (sticky-header)
-      (works-circles)]
+      (sticky-header)]
     (footer)
     [:div.copyright-71f95 "&copy2015 daniel martin "
       [:span.lowercase-de065 "del "] "campo"])
 
-(dom/set-html! "content" (body))
+(set-html! "content" (body))
